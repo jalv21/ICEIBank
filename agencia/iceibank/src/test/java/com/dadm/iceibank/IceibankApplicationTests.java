500a68316a10f3a18f6e4e97f836ba597ab3cde2
Jalv <araujo.ja.rodrigues@gmail.com>
2026-08-29 11:24:52 -0300

    config: criando projeto Spring Boot

---
+++ agencia/iceibank/src/test/java/com/dadm/iceibank/IceibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadm.iceibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
